5dd5f052bfb9dc8b3f361405ffb8e283a5a9bc61
Shruti Singh <singhshruti2029@gmail.com>
2026-08-19 01:29:26 +0530

    Pattern3 and pattern4

---
+++ src/Patterns.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Patterns {
+
+    // Pattern 3
+    static void pattern3(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+
+    // pattern 4
+    static void pattern4(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+
+
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter n: ");
+        int n = sc.nextInt();
+
+       // pattern3(n);
+      //  pattern4(n);
+
+        sc.close();
+    }
+}
