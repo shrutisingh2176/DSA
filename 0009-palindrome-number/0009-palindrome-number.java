4453640e11e32efd95e765485ad30439e73567aa
Shruti Singh <singhshruti2029@gmail.com>
2026-08-25 01:53:00 +0530

    Time: 4 ms (100%), Space: 45.8 MB (87.18%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+       
+       int dup = x;
+       long rev =0;
+       while (x>0) {
+        int digit = x%10;
+        rev = rev*10+digit;
+        x=x/10;
+         }   
+         if (rev==dup) {
+           return true;
+         } else {
+            return false;
+         }    
+    }
+}
